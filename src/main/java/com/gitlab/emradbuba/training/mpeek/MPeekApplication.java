5e11abef37b6854444160082b0356b4ded77093d
Radek Em <3488909-emradbuba@users.noreply.gitlab.com>
2021-09-18 20:16:34 +0200

    Initial commit with basic structure
    Signed-off-by: Radek Em <3488909-emradbuba@users.noreply.gitlab.com>

---
+++ src/main/java/com/gitlab/emradbuba/training/mpeek/MPeekApplication.java
@@ -0,0 +1,13 @@
+package com.gitlab.emradbuba.training.mpeek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MPeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MPeekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gitlab/emradbuba/training/mpeek/MPeekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitlab.emradbuba.training.mpeek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MPeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
